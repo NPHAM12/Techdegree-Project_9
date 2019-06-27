a89616fb68638a92c8e9e29ea067fe4596501c80
Billy <40702630+NPHAM12@users.noreply.github.com>
2019-06-27 14:45:06 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -8,22 +8,16 @@ const {User} = require('../models');
 
 // GET /api/users 200 - Returns the currently authenticated user
 router.get("/", auth, (req, res) => {
-  User.findAll({
-    attributes: [
-      'id',
-      'firstName',
-      'lastName',
-      'emailAddress',
-    ]
-  })
-  .then((allUsers) => {
-    res.status(200).json({
-      "users": allUsers
+  const user = req.currentUser;
+  const currentUser={
+    id: user.id,
+    firstName: user.emailAddress,
+    lastName: user.lastName,
+    emailAddress: user.emailAddress
+  }
+  res.status(200).json({
+      "user": currentUser
     });
-  })
-  .catch((err) => {
-    next(err); //pass any Sequelize validation errors to the global error handler.
-  });
 });
 
 // POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
@@ -31,10 +25,14 @@ router.post("/", (req, res, next) => {
   const newUser = req.body;
   // console.log(newUser);
   //Check all inputs
-  if (newUser.firstName == false
-      || newUser.lastName == false
-      || newUser.emailAddress == false
-      || newUser.password == false) {
+  // if (newUser.firstName == false
+  //     || newUser.lastName == false
+  //     || newUser.emailAddress == false
+  //     || newUser.password == false) {
+  // the condition "above" will be bad when one of conditions is "undefined"
+
+  if (!newUser.firstName || !newUser.lastName
+      || !newUser.emailAddress || !newUser.password){
     const err = new Error("Missing firstName, lastName, emailAddress or password!");
     err.status = 400;
     next(err);//pass any Sequelize validation errors to the global error handler.
@@ -52,12 +50,12 @@ router.post("/", (req, res, next) => {
           //Create a new user
           User.create(newUser)
             .then(() => {
-              res.location('/api/users/' + newUser.id);  //Set location header
+              res.location('/');  //Set location header
               res.status(201).end();
             })
             //Catch error and check if Sequelize validation  error (not using) and pass error to next middleware
             .catch(err => {
-              err.status = 500;
+              err.status = 400;
               next(err); //pass any Sequelize validation errors to the global error handler.
             });
         }else { //if email exists
